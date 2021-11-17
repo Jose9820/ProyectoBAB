1a95d7cf41fa30df3852153695e4d1c826bffae5
José Antonio <pliegogomezj@gmail.com>
2021-11-16 23:15:38 -0600

    master

--- login.php
+++ login.php
@@ -3,11 +3,12 @@ $nombre = $_POST['nombreUsuario'];
 $contrasena = $_POST['contrasenaUsuario'];
 
 require 'conexionBD.php';
-$consulta = $conexion->query("SELECT Tipo_Usuario FROM usuarios WHERE Nombre_Usuario = '$nombre' AND Contrasena = '$contrasena'");
+$consulta = $conexion->query("SELECT ID_usuarios, Tipo_Usuario FROM usuarios WHERE Nombre_Usuario = '$nombre' AND Contrasena = '$contrasena'");
 if(mysqli_num_rows($consulta) > 0){
     $resultado = mysqli_fetch_array($consulta);
-    echo"Su cuenta es de tipo: $resultado[0]";
-    //header("Location: bienvenida.html");
+    $_SESSION["id"]= $resultado[0];
+    $_SESSION["tipoCuenta"]= $resultado[1];
+    header("Location: bienvenida.html");
 }else{
     echo "Esta cuenta no existe";
 }
