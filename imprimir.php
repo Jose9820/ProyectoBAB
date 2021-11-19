d688fb2dc1076e483198ae7d5dbbe72553e8edde
José Antonio <pliegogomezj@gmail.com>
2021-11-18 23:37:27 -0600

    descargar boleto en formato pdf

--- imprimir.php
+++ imprimir.php
@@ -6,21 +6,30 @@ require 'conexionBD.php';
 
 $consulta = $conexion->query("SELECT * FROM `boletos` WHERE ID_boleto='$idBoleto' AND id_Usuario='$_SESSION[id]'");
 if(mysqli_num_rows($consulta) > 0){
+    $boleto = mysqli_fetch_array($consulta);
+    $usuario = mysqli_fetch_array($conexion->query("SELECT Nombre_Usuario FROM `usuarios` WHERE ID_usuarios='$_SESSION[id]'"));
+    $recorrido = mysqli_fetch_array($conexion->query("SELECT Lugar_Partida, Lugar_Destino FROM recorridos WHERE ID_recorridos='$boleto[Codigo]'"));
+    
     $pdf = new FPDF();
     $pdf->AddPage();
     $pdf->SetFont('Arial','B',16);
-    $pdf->Cell(40,10,'Boleto de viaje');
-    while($row = $consulta ->fetch_assoc())
-    {
-        $pdf -> Cell (20, 10, $row ['Codigo'], 1 , 0, 'C', 0);
-        $pdf -> Cell (30, 10, $row ['Nombre_Usuario'], 1 , 0, 'C', 0);
-        $pdf -> Cell (20, 10, $row ['Lugar_Partida'], 1 , 0, 'C', 0);
-        $pdf -> Cell (20, 10, $row ['Lugar_Destino'], 1 , 0, 'C', 0);
-        $pdf -> Cell (20, 10, $row ['Hora'], 1 , 0, 'C', 0);
-        $pdf -> Cell (20, 10, $row ['Fecha'], 1 , 0, 'C', 0);
-        $pdf -> Cell (20, 10, $row ['Precio'], 1 , 0, 'C', 0);
-        $pdf -> Cell (20, 10, $row ['Cupo_Disponible'], 1 , 1, 'C', 0);
-    }
+    $pdf->Cell(40,10,'Boleto de viaje', 0, 1, "C");
+    $pdf->SetFont('Arial','',16);
+    $pdf->Cell(40, 10, 'Codigo:', "LT", 0, 'L');
+    $pdf->Cell(40, 10, $boleto['Codigo'],"T", 0, 'L');
+    $pdf->Cell(40, 10, 'Nombre:', "T", 0, 'L');
+    $pdf->Cell(40, 10, $usuario[0], "TR", 1, 'L');
+    $pdf->Cell(40, 10, 'Origen:', "L", 0, 'L');
+    $pdf->Cell(40, 10, $recorrido[0], 0, 0, 'L');
+    $pdf->Cell(40, 10, 'Destino:', 0, 0, 'L');
+    $pdf->Cell(40, 10, $recorrido[1], "R", 1, 'L');
+    $pdf->Cell(40, 10, 'Hora:', "L", 0, 'L');
+    $pdf->Cell(40, 10, $boleto['Hora_Compra'], 0, 0, 'L');
+    $pdf->Cell(40, 10, 'Fecha:', 0, 0, 'L');
+    $pdf->Cell(40, 10, $boleto['Fecha_Compra'], "R", 1, 'L');
+    $pdf->Cell(80, 10, 'Precio:', "LB", 0, 'L');
+    $pdf->Cell(80, 10, '$'.$boleto['Precio_Boleto'], "BR", 0, 'L');
+    
     $pdf->Output();
     //header("location: boletos.php");
 }else{
